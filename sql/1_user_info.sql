a7a04833fda22126ce05846751618255d3d80ad1
Alexandria Wang <94774823@qq.com>
2020-02-14 23:55:16 +0800

    新增用户表

---
+++ sql/1_user_info.sql
@@ -0,0 +1,15 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS  `user_info`;
+CREATE TABLE `user_info` (
+  `employeeId` varchar(32) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `group` varchar(32) DEFAULT NULL,
+  `role` varchar(32) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+
+SET FOREIGN_KEY_CHECKS = 1;
+
