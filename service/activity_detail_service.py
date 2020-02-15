3c3661504cc48b70db9d08d866bdd81c0dd115dc
Youngman <taifeiyang@126.com>
2020-02-15 18:56:40 +0800

    新增订餐明细接口

--- service/activity_detail_service.py
+++ service/activity_detail_service.py
@@ -3,7 +3,7 @@ import datetime
 from config.config import DB_HOST, DB_USER, DB_PWD, DB_PORT
 
 
-# 插入活动明细
+# 新建活动明细
 def create_activity_detail(activity_id, employee_id, quantity, created_by):
 
     db = pymysql.connect(host=DB_HOST,
@@ -32,3 +32,33 @@ def create_activity_detail(activity_id, employee_id, quantity, created_by):
     finally:
         db.close()
 
+
+# 查询用户所有的订餐明细
+def query_all_activity_detail_by_eid(employee_id):
+
+    db = pymysql.connect(host=DB_HOST,
+                         port=DB_PORT,
+                         user=DB_USER,
+                         password=DB_PWD,
+                         database="OrderFoodAssistant",
+                         charset="utf8")
+
+    try:
+        # 新建游标
+        with db.cursor() as cursor:
+            # 执行sql语句
+            sql = "SELECT * FROM activity_detail WHERE employeeId = %s"
+            values = (employee_id)
+            cursor.execute(sql, values)
+            # 取第一条
+            data = cursor.fetchall()
+            print(data)
+            cursor.close()
+            return data
+    finally:
+        db.close()
+
+
+# 以下是调试用的
+if __name__ == '__main__':
+    query_all_activity_detail_by_eid("660001")
