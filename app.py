67dc55062428a7e5e4b13506d0e35790786b6b27
lgtian <350409384@qq.com>
2020-02-15 11:39:01 +0800

    add login.html

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = '6789023yhfkjasd234'
 
 
-@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    if request.method == 'POST':
+        flash('用户名或密码不正确')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
